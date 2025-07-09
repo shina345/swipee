ba1fb92500dc38511f52da8eb7007f8bfeea1c63
shina345 <155635864+shina345@users.noreply.github.com>
2025-07-09 21:11:03 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/Link';
 import { useState } from 'react';
 
 const Home: NextPage = () => {
